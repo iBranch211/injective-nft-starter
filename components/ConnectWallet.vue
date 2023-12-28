78f9bf5158c1b5159349894c773583f806992499
Branch <108258871+iBranch211@users.noreply.github.com>
2023-12-28 11:34:47 -0500

    tailwind css style add

--- components/ConnectWallet.vue
+++ components/ConnectWallet.vue
@@ -1,7 +1,22 @@
 <script setup>
 import { useWalletStore } from "~~/store/wallet";
+import {useModalStore} from "~/store/modal";
+import {Modal, WalletConnectStatus, WalletModalType} from "~/types";
+import {Status, StatusType} from "@injectivelabs/utils";
 
+const modalStore = useModalStore()
 const walletStore = useWalletStore();
+
+const status = reactive(new Status(StatusType.Loading))
+const walletModalType = ref(WalletModalType.All)
+
+const isModalOpen = computed(
+    () => modalStore.modals[Modal.Connect]
+)
+
+const isLoading = computed(
+    () => walletStore.walletConnectStatus === WalletConnectStatus.connecting
+)
 
 const injAddressShort = computed(
   () =>
@@ -12,8 +27,15 @@ const injAddressShort = computed(
 );
 
 function handleClick() {
-  walletStore.connectWallet().catch(alert);
+
+  // walletStore.connectWallet().catch(alert);
+  modalStore.openModal(Modal.Connect)
 }
+
+function onCloseModal() {
+  modalStore.closeModal(Modal.Connect)
+}
+
 </script>
 
 <template>
@@ -24,4 +46,23 @@ function handleClick() {
   >
     Connect Wallet
   </Button>
+
+  <AppModal
+      :is-open="isModalOpen"
+      :is-loading="isLoading"
+      :ignore="['.v-popper__popper']"
+      is-md
+      @modal:closed="onCloseModal"
+  >
+    <template #title>
+      <h3>
+        Connect To Wallet
+      </h3>
+    </template>
+
+    <ul
+        class="divide-y divide-gray-800 border-gray-700 rounded-lg max-h-[65vh]"
+    >
+    </ul>
+  </AppModal>
 </template>
--- nuxt-config/vite.ts
+++ nuxt-config/vite.ts
@@ -1,28 +1,35 @@
-import { UserConfig, defineConfig } from "vite";
-import { nodePolyfills } from "@bangjelkoski/vite-plugin-node-polyfills";
+import {defineConfig, UserConfig} from "vite";
+import {nodePolyfills} from "@bangjelkoski/vite-plugin-node-polyfills";
+import AutoImport from "unplugin-auto-import/vite"
 
 export default defineConfig({
-  define: {
-    "process.env": JSON.stringify({}),
-    "process.env.DEBUG": JSON.stringify(process.env.DEBUG),
-  },
-
-  plugins: [nodePolyfills({ protocolImports: false })],
-
-  build: {
-    sourcemap: false,
-
-    rollupOptions: {
-      cache: false,
-      output: {
-        manualChunks: (id: string) => {
-          //
-        },
-      },
+    define: {
+        "process.env": JSON.stringify({}),
+        "process.env.DEBUG": JSON.stringify(process.env.DEBUG),
     },
-  },
 
-  optimizeDeps: {
-    exclude: ["fsevents"],
-  },
+    plugins: [
+        nodePolyfills({protocolImports: false}),
+        AutoImport({
+            dirs: ['./composables', './store/', './store/**/index.ts'],
+            imports: ['vue', '@vueuse/core']
+        })
+    ],
+
+    build: {
+        sourcemap: false,
+
+        rollupOptions: {
+            cache: false,
+            output: {
+                manualChunks: (id: string) => {
+                    //
+                },
+            },
+        },
+    },
+
+    optimizeDeps: {
+        exclude: ["fsevents"],
+    },
 }) as UserConfig;
